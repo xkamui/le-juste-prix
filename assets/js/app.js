31fe2e149e2194b2f3e511797e1087f69f77ee87
Kamui Shirou <x_kamui@msn.com>
2023-12-12 14:47:37 +0100

    Factorizing some, and making things pretty on js and css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,28 @@
+//## Most used elements
+const splashScreen = document.querySelector('#splashScreen')
+const startButton  = document.querySelector('#startGame')
+
 let justePrix = null
 const minPrice = 0
 const maxPrice = 2000
 
-document.querySelector('#startGame').addEventListener('click', function() {
+startButton.addEventListener('click', function() {
+
+    justePrix = getRandomNumber()
+    splashScreen.classList.add('closed')
+    console.log(justePrix)
+
+}, false)
+
+
+
+
+
+function getRandomNumber() {
 
     min = Math.ceil(minPrice)
     max = Math.floor(maxPrice)
 
-    justePrix = Math.floor(Math.random() * (max - min + 1)) + min
+    return Math.floor(Math.random() * (max - min + 1)) + min
 
-    console.log(justePrix)
-
-}, false)
+}
